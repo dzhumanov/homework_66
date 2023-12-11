3f1fc9c92a3cb9645003e4eb75586984239c3979
Temirlan Dzhumanov <temadjumanov@gmail.com>
2023-12-11 17:21:06 +0600

    Created layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import "./App.css";
-import Toolbar from "./components/Toolbar/Toolbar";
+import Layout from "./components/Layout/Layout";
 import { Routes, Route } from "react-router-dom";
 import Meals from "./containers/Meals/Meals";
 import MealForm from "./containers/MealForm/MealForm";
 
 function App() {
   return (
-    <>
-      <header>
-        <Toolbar />
-      </header>
-      <main className="container-fluid">
-        <Routes>
-          <Route path="/" element={<Meals />} />
-          <Route path="meal-form/" element={<MealForm />} />
-          <Route path="meal-form/:id" element={<MealForm />} />
-          <Route path="*" element={<h1>Not Found!</h1>} />
-        </Routes>
-      </main>
-    </>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Meals />} />
+        <Route path="meal-form/" element={<MealForm />} />
+        <Route path="meal-form/:id" element={<MealForm />} />
+        <Route path="*" element={<h1>Not Found!</h1>} />
+      </Routes>
+    </Layout>
   );
 }
 
